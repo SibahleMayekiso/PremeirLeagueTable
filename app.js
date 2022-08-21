5c126da8232b31f8c5df59f445f3b43b7a161939
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-08-21 16:51:02 +0200

    Creating Project skeleton

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}!`);
+});
+
+app.get('/', (req, res) => {
+    res.render('index', () =>{
+        console.log('Page loaded successfully');
+    })
+})
