d199da0a8420fc94eccd90ae47ef090b96c8e4c6
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-08-21 18:59:17 +0200

    Formatting the footer items

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}!`);
