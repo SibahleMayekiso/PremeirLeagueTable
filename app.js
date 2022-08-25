aa24f1cd4cfac4616362506593ef2dfeaf944d4d
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-08-25 17:36:41 +0200

    Extracting spreadsheet data

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+//File handling
+const fs = require('fs');
+const xlsx = require('xlsx');
+
 const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
@@ -11,6 +15,14 @@ app.listen(port, () => {
     console.log(`Server listening on port ${port}!`);
 });
 
+const workbook = xlsx.readFile('./public/EnglishPremierLeague_Workbook.xlsx');
+
+let worksheets = {};
+for (const sheet of workbook.SheetNames) {
+    worksheets[sheet] = xlsx.utils.sheet_to_json(workbook.Sheets[sheet]);
+}
+
+console.log("JSON: \n", JSON.stringify(worksheets.PremierLeague2122Table), "\n\n");
 app.get('/', (req, res) => {
     res.render('index')
 })
