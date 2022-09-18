d02120ffeade7047903e0d68efdf977d41d4941e
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-09-18 15:27:00 +0200

    Schema setup

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const Team = require("../models/team");
+
+const port = process.env.PORT || 3000;
+const url = process.env.DATABASE_CONN_URL;
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+//DB Setup
+const mongoose = require("mongoose");
+mongoose.connect(url);
+const selectDB = mongoose.connection;
+selectDB.on("error", () => {
+  console.log("An error occured! Please try again later.");
+});
+selectDB.once("open", () => {
+  console.log("Database successfully opened");
+});
 
 //File handling
 const fs = require("fs");
@@ -10,21 +28,20 @@ let worksheets = {};
 for (const sheet of workbook.SheetNames) {
   worksheets[sheet] = xlsx.utils.sheet_to_json(workbook.Sheets[sheet]);
 }
-
-const port = process.env.PORT || 3000;
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
+//DB Data storage
+for (const value of worksheets) {
+  //Do something
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
 });
 
-console.log(
-  "JSON: \n",
-  JSON.stringify(worksheets.PremierLeague2122Table),
-  "\n\n"
-);
+// console.log(
+//   "JSON: \n",
+//   JSON.stringify(worksheets.PremierLeague2122Table),
+//   "\n\n"
+// );
 app.get("/", (req, res) => {
   res.render("index");
 });
--- models/team.js
+++ models/team.js
@@ -1,1 +1,28 @@
 //Database Schema setup
+const mongoose = require("mongoose");
+
+const teamSchema = new mongoose.Schema({
+  position: {
+    type: Number,
+  },
+  team: {
+    type: String,
+  },
+  gamesPlayed: {
+    type: Number,
+  },
+  goalsScored: {
+    type: Number,
+  },
+  goalsConceeded: {
+    type: Number,
+  },
+  goalDifference: {
+    type: Number,
+  },
+  points: {
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model("TeamSchema", teamSchema);
